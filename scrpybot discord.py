7b5838b79c7d7a414792963f6add6b45e2c8938f
R3iZer0 <94191193+R3iZer0@users.noreply.github.com>
2024-01-21 22:39:11 +0100

    Update scrpybot discord.py

--- scrpybot discord.py
+++ scrpybot discord.py
@@ -2,8 +2,8 @@ import discord
 import pyautogui
 from discord.ext import tasks, commands
 
-TOKEN = 'MTE5MjIyNjQwOTE4NDIzNTUzMA.Glh4tV.l0nJuKQQ5MSKlNPRYGsfV3jwNBKhBfK2JICxVQ'
-CHANNEL_ID = 1192218503692959747  # Replace with your channel ID
+TOKEN = ''
+CHANNEL_ID =  # Replace with your channel ID
 
 intents = discord.Intents.default()
 intents.typing = False
